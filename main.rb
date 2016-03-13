3bb72fe3666a290b50297dc82f3e378922044b2c
Guoliang Cao <gcao99@gmail.com>
2016-03-13 10:43:24 -0400

    Clean up, externalize stomp config

--- main.rb
+++ main.rb
@@ -1,7 +1,11 @@
-#require 'bunny'
 require 'stomp'
 require 'json'
 require 'securerandom'
+
+ENV["STOMP_SERVER"]   ||= "subscribe-vm-06.dloco.s.vonagenetworks.net"
+ENV["STOMP_PORT"]     ||= "61613"
+ENV["STOMP_USERNAME"] ||= "admin"
+ENV["STOMP_PASSWORD"] ||= "admin"
 
 ABOUT_ME = {
   name: "Mr. Smith",
@@ -20,71 +24,7 @@ WHAT_CAN_I_DO = {
   ],
 }
 
-#SERVER   = ENV['AMQP_SERVER']   || 'localhost'
-#USERNAME = ENV['AMQP_USERNAME'] || 'guest'
-#PASSWORD = ENV['AMQP_PASSWORD'] || 'guest'
-
-#conn = Bunny.new "amqp://#{USERNAME}:#{PASSWORD}@#{SERVER}"
-#conn.start
-
-#puts "Connected to #{SERVER} as #{USERNAME}"
-
-#ch = conn.create_channel
-
-##requests_x = ch.fanout("requests-x", durable: true)
-#requests = ch.queue("requests", "auto-delete" => true, arguments: {'x-message-ttl' => 30000, 'x-dead-letter-exchange' => 'dead-requests-x'})
-##requests.bind(requests_x)
-
-#tasks_x = ch.headers("tasks-x", durable: true)
-#tasks = ch.queue("tasks", "auto-delete" => true, arguments: {'x-message-ttl' => 60000, 'x-dead-letter-exchange' => 'dead-tasks-x'})
-#tasks.bind(tasks_x)
-
-##responses_x = ch.fanout("responses-x", durable: true)
-#responses = ch.queue("responses", "auto-delete" => true, arguments: {'x-message-ttl' => 60000})
-##responses.bind(responses_x)
-
-#requests.subscribe(manual_ack: true) do |delivery_info, metadata, payload|
-#  parsed = JSON.parse payload
-
-#  if parsed["message"] =~ /check disk space/i
-#    puts "=" * 80
-#    puts "Check disk space request is received"
-#    message = {
-#      id: SecureRandom.uuid,
-#      source: parsed["source"],
-#      type: 'check-disk-space',
-#      server: 'TODO',
-#    }
-#    tasks_x.publish(message.to_json, routing_key: 'tasks')
-#    ch.ack delivery_info.delivery_tag, false
-#  else
-#    puts "Rejected: #{parsed["message"]}"
-#    ch.reject delivery_info.delivery_tag, true
-#  end
-
-#  puts "Done with the request"
-#end
-
-#tasks.subscribe(manual_ack: true) do |delivery_info, metadata, payload|
-#  puts payload
-#  parsed = JSON.parse payload
-
-#  if parsed["type"] == "check-disk-space"
-#    message = {
-#      source: parsed['source'],
-#      content: `df`,
-#    }
-#    responses.publish(message.to_json, routing_key: 'responses')
-#    ch.ack delivery_info.delivery_tag, false
-#  else
-#    puts "Rejected: #{parsed["type"]}"
-#    ch.reject delivery_info.delivery_tag, true
-#  end
-
-#  puts "Done with the task"
-#end
-
-client = Stomp::Client.new("stomp://admin:admin@localhost:61613")
+client = Stomp::Client.new("stomp://#{ENV["STOMP_USERNAME"]}:#{ENV["STOMP_PASSWORD"]}@#{ENV["STOMP_SERVER"]}:#{ENV["STOMP_PORT"]}")
 
 client.subscribe "/topic/requests" do |msg|
   begin
